3a53487604d8055465a9c270cd5cb4e63a78338b
John <jvohnny@gmail.com>
2017-06-15 23:27:36 +0700

    validation

--- model.py
+++ model.py
@@ -2,30 +2,41 @@ import numpy as np
 
 print "running model"
 
-def guess(w, nth_week, cases_for_prediction, degree):
+def guess(all_ws, nth_week, cases_for_prediction, temps_for_prediction):
+
+    n_omega = 17 # lag + 1
+    n_season = 2
+    n_temp = 10 # 9 weeks temp + 1
+    # n_rain = 
+    # n_humid = 
     
-    def omega(w, cases_for_prediction):
+    def omega(w_omega, cases_for_prediction):
         pad_y = np.concatenate(([1], np.array(cases_for_prediction)))
-        weights = np.array(w[:-4])
+        weights = np.array(w_omega)
         positive_weights = weights**2 # force the weights to be positive
         all_cases = np.dot(positive_weights,pad_y)
         return all_cases
 
     # there's no amplitude in the season since the omega is absorbed in the above f'n
-    def season_sin(w, nth_week):
+    def season_sin(w_season, nth_week):
         pi = np.math.pi
         coeff = pi/26.
-        shift_phase = w[-3]
+        shift_phase = w_season[1]
         inside = coeff*(nth_week - shift_phase)
         sine = np.math.sin(inside)
-        c_sq = w[-4]**2
+        c_sq = w_season[0]**2
         score = 1 + c_sq + sine # avoid going to negative
         return score    
 
-    def temperature_term(w, degree):
-        beta = w[-2]
-        c = w[-1]
-        return beta*degree + c
+    def temperature_term(w_temp, temps_for_prediction):
+        # print "len temps_for_prediction",len(temps_for_prediction)
+        pad_y = np.concatenate(([1], np.array(temps_for_prediction)))
+        # print "len py",len(pad_y)
+        # print "py",pad_y
+        # print "w",len(w_temp)
+        weights = np.array(w_temp)
+        all_temps = np.dot(weights,pad_y)
+        return all_temps
 
     def rain_term(w, rain):
         return
@@ -33,9 +44,18 @@ def guess(w, nth_week, cases_for_prediction, degree):
     def humidity_term(w, humidity):
         return
     
-    omega = omega(w, cases_for_prediction)
-    seasonality_part = season_sin(w, nth_week)
-    temperature_part = temperature_term(w, degree)
+    w_omega = all_ws[:n_omega]
+    # print "omega",w_omega
+    omega = omega(w_omega, cases_for_prediction) # takes the first part of w
+
+    w_season = all_ws[n_omega:n_omega+n_season]
+    # print "season",w_season
+    seasonality_part = season_sin(w_season, nth_week) # takes the second part of w
+
+    w_temp = all_ws[n_omega+n_season:]
+    # print "temps_for_prediction",temps_for_prediction
+    temperature_part = temperature_term(w_temp, temps_for_prediction) # takes the third part of w
+
     # rain_part = rain_term(w, rain)
     # humidity_part = humidity_term(w, humidity)
 
--- weather_alphas.py
+++ weather_alphas.py
@@ -5,46 +5,52 @@ import weather_costs
 print 'getting bounded alphas ...'
 
 # train comes in the form of dataframe
-def get_alphas(LAG, train, week_forward = 16):
+def get_alphas(LAG, TEMPERATURE_WEEKS, train, week_forward = 16):
     W_CASE = LAG + 1
-    W_POP = 1
     W_SEASON = 2
-    BETAS_SET = W_POP + W_CASE + W_SEASON # addition of ALL_WS betas
+    W_TEMP = 10
+    BETAS_SET = W_TEMP + W_CASE + W_SEASON # addition of ALL_WS betas
 
     # use poison instead of least square
     def cost(w):
-        start_week,end_week = 0,LAG
-        predicted_line_offset = week_forward + end_week - 1 # 41
+        start_week, end_cases_week, end_temp_week = 0, LAG, TEMPERATURE_WEEKS
+        predicted_line_offset = week_forward + end_cases_week - 1 # 41
         
         all_penalties = []
         for real_week in range(len(train)-predicted_line_offset):
-            cases_for_pred = train.cases[start_week:end_week]
+            cases_for_prediction = train.cases[start_week:end_cases_week]
+            temps_for_prediction = train.meantemp[start_week:end_temp_week]
 
             cur_penalty = weather_costs.nweek_ahead_cost(
                 w, 
                 week_forward, 
                 start_week, 
-                end_week, 
+                end_cases_week, 
                 train, 
-                cases_for_pred)
+                cases_for_prediction,
+                temps_for_prediction)
 
             all_penalties.append(cur_penalty)
             start_week+=1
-            end_week+=1
+            end_cases_week+=1
+            end_temp_week+=1
             
         return np.sum(all_penalties) / float(len(all_penalties))
 
+    head_arr = np.array([0.05]*W_CASE)
     seasonality_starters = [0.75,20] # [constant,phase]
-    temperature_starters = [0.,0.]
-    head_arr = np.array([0.05]*W_CASE) 
+    temperature_starters = [0.]*W_TEMP 
+
     arr = np.append(head_arr,seasonality_starters) #  lag + 1 + 2 betas
     all_arr = np.append(arr,temperature_starters) # lag + 1 + 2 + 2 betas
     bnds = [(0.,None)]*W_CASE
-    tail_bnds = [(None.,None),(None,None),(0.,None),(None,None)]
+    tail_bnds = [(None,None),(1.,None)]
     bnds.extend(tail_bnds)
-    # print "bnds",len(bnds),bnds
-    # print "arr",len(arr),arr
-    w = minimize(cost,all_arr,bounds = bnds)
+    [bnds.extend([(None,None)]) for i in range(W_TEMP)]
+    print "bnds",len(bnds),bnds
+    print "arr",len(all_arr),all_arr
+    myfactr = 1e2
+    w = minimize(cost,all_arr,bounds = bnds,options={'ftol' : myfactr * np.finfo(float).eps})
     return w
 
 print "done getting alphas ..."
